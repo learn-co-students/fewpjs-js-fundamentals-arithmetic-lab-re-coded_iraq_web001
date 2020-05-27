4be6b2135fbab6ecf308401b33f0805b3e66f3b1
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-05-27 05:43:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+console.log(multiply);
+var random = (Math.random()*10)
+console.log(random);
+var mod = 24 % 5
+console.log(mod);
+var max = Math.max(12,13,5,20)
